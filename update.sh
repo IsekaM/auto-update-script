47fa13acab5cf007d3b8c87f2323d02dfd020787
Makesi Morrison <makmorrison@gmail.com>
2018-11-14 15:41:14 -0500

    Updated script. Update verification now works perfectly.

--- update.sh
+++ update.sh
@@ -1,23 +1,41 @@
-#!/usr/bin/env bash
+#!/bin/bash
+###############################################################################
+# This is a script to automatically update Ubuntu system/server
+###############################################################################
 
-#This is a script to automatically update Ubuntu system/server
 
+### Varibles
+upToDate="All packages are up to date."
+logName="check-update.txt"
+
+echo "$upToDate"
+
+### Functions
 divider() {
   echo -e "\n================================================================\n"
 }
 
+## Function to remove check-update.txt file
+removeLog() {
+  rm $logName
+}
+
+
+## Function to refresh repo database
 refreshDB() {
   echo "Refreshing repository database..."
   sleep 2
-  apt update
+  apt update | tee $logName
   divider
 }
 
-checkUpdate() {
-  upgrades="$(refreshDB | grep -ow 'All packages are up to date.')"
-  echo $upgrades
 
-  if [[ ! -z "$upgrades" ]]
+## Function to check if the system needs to be updated or if should be.
+checkUpdate() {
+  # Store string from file into var 'upgrades'
+  upgrades=`cat $logName | grep -ow "$upToDate"`
+
+  if [ "$upgrades" == "$upToDate" ]
   then
     echo "Upgrading system..."
     apt upgrade -y
@@ -29,13 +47,19 @@ checkUpdate() {
 
     echo -e 'Rebooting system...'
     divider
+
+    removeLog
     reboot
 
   else
+    removeLog
     echo "No updates available."
+    divider
   fi
 }
 
+
+## Functions to initialize commands if the user is root
 update() {
   if [ $USER == "root" ]
   then
